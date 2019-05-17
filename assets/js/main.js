5cd588bff18dcff6057f2a8f9e446a366b93e88b
Qasim Qureshi <qasim.qureshi@gmail.com>
2019-05-17 17:33:36 +0500

    Location detection, UI updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+geoip2.city (
+  // success handler
+  function(data){
+    mapboxgl.accessToken = 'pk.eyJ1IjoicWFzaW01NyIsImEiOiJjanZzMTN4YmYwbTJoNDRtc3lveTUycjR5In0.NHo5uv7_XQpM7fPEus_M-w';
+    var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+      center: [data.location.longitude, data.location.latitude], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+    
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      }));
+},
+  // error
+  function(data) {
+    // error loading location
+})
+
 document.addEventListener('DOMContentLoaded', (event) => {
   
   // Ensuring that anchors with an href of '#' don't cause the page to jump
