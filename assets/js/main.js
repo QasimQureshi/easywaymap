98b19b999c030060715a5d1aae9d8f97c4b434b9
Qasim Qureshi <qasim.qureshi@gmail.com>
2019-05-17 17:39:04 +0500

    Using a generic location if the IP based location fails

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,13 @@ geoip2.city (
 },
   // error
   function(data) {
-    // error loading location
+    // error loading location. Using a generic location
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+    });
 })
 
 document.addEventListener('DOMContentLoaded', (event) => {
